4da10311b64fbfdbe0fa88f6cba320fc565eee14
Kumaran Baskaran <kbaskaran2@wisc.edu>
2015-10-29 09:18:20 -0500

    Initial Commit

---
+++ MeasureDistance/MeasureDistance/Distance.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 29, 2015
+
+@author: kbaskaran
+'''
+
+class Distance():
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
