4f39aec95b26f1b4cd200a7cf61f3c2311a1b80c
Kumaran Baskaran <kbaskaran2@wisc.edu>
2017-03-24 10:32:43 -0500

    RandomRunner added

---
+++ ObstacleAvoidance/ObstacleAvoidance/RandomRunner.py
@@ -0,0 +1,156 @@
+'''
+Created on Mar 25, 2016
+
+Created on Jan 27, 2016
+3.3V pin : 1,17
+5V pin : 2,4
+Ground : 6,9,14,20,25,30,34,39
+EPROM : 27,28
+GPIO : 3,5,7,8,10,11,12,13,15,16,18,10,21,22,23,24,26,29,31,32,33,35,36,37,38,40
+
+Motor Control : 29,31,33,35
+front 7,8
+left 11,12
+right 15,16
+back 21,22
+top 23,24
+signal 26
+sigt 10
+
+
+wireless IMU
+import socket, traceback
+
+host = ''
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind((host, port))
+
+while 1:
+try:
+message, address = s.recvfrom(8192)
+print message
+except (KeyboardInterrupt, SystemExit):
+raise
+except:
+traceback.print_exc()
+@author: kumaran
+
+@author: kumaran
+'''
+
+import RPi.GPIO as GPIO
+import time
+from string import atoi,atof
+import sys,tty,termios
+#import getch
+import random
+import socket
+
+
+
+class EchoSensor(object):
+    
+    def __init__(self,trigger,echo):
+        self.trigger = trigger
+        self.echo = echo
+        #print "Sensor configured with t,e",self.trigger,self.echo
+        GPIO.setup(self.trigger,GPIO.OUT)
+        GPIO.setup(self.echo,GPIO.IN,pull_up_down = GPIO.PUD_DOWN)
+        time.sleep(0.5)
+        
+    def measure(self):
+        GPIO.output(self.trigger,0)
+        #GPIO.input(self.echo,pull_up_down = GPIO.PUD_DOWN)
+        GPIO.output(self.trigger,True)
+        time.sleep(0.00001)
+        GPIO.output(self.trigger,False)
+        self.startTime=time.time()
+        while GPIO.input(self.echo) == False:
+            self.startTime = time.time()
+        while GPIO.input(self.echo) == True:
+            self.stopTime = time.time()
+        self.elapsedTime=self.stopTime-self.startTime
+        self.distance=(self.elapsedTime*34000.0)/2.0
+        return self.distance
+    
+    
+    def avgDistance(self,trails):
+        self.avgdist=0.0
+        for i in range(trails):
+            time.sleep(0.1)
+            self.avgdist+=self.measure()
+        return self.avgdist/trails
+    
+class Engine(object):
+    
+    def __init__(self,lm1,lm2,rm1,rm2,t,dc,ft=0,fc=0,bt=0,bc=0):
+        self.status="x"
+        self.turnDelay=t
+        self.leftMotor=[lm1,lm2]
+        self.rightMotor=[rm1,rm2]
+        self.motors=self.leftMotor+self.rightMotor
+        self.DistanceCutoff=dc
+        self.DepthCutoff=100.00
+        GPIO.setup(self.motors,GPIO.OUT)
+        if ft and fc:
+            self.FronSensor=True
+            self.FS=EchoSensor(ft,fc)
+        else:
+            self.FronSensor=False
+        if bt and bc:
+            self.BackSensor=True
+            self.BS=EchoSensor(bt,bc)
+        else:
+            self.BackSensor=False
+            
+      
+    def Scan(self):
+        if self.FronSensor and self.BackSensor:
+            self.FS.measure()
+            self.BS.measure()
+        else:
+            self.FS.measure()
+    
+    def Stop(self):
+        self.status='s'
+        GPIO.output(self.motors,0)
+      
+    def RandomTurn(self):
+        direction = random.choice(['L','R'])
+        turndelay = random.choice([0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5])
+        if direction == "L":
+            self.status = 'l'
+            GPIO.output(self.motors,(0,0,1,0))
+            time.sleep(turndelay)
+            self.Stop()
+        else:
+            self.status = 'r'
+            GPIO.output(self.motors,(0,1,0,0))
+            time.sleep(turndelay)
+            self.Stop()
+    def RandomRun(self):
+        blockCount=0
+        while (blcokCount<5):
+            self.Scan()
+            if self.FS.distance < self.DepthCutoff or self.BS.distance > self.DepthCutoff:
+                self.Stop()
+            else:
+                blockCount=0
+                self.status='f'
+                GPIO.output(self.motors,(0,1,1,0))
+            if self.status == 's':
+                blockCount+=1
+                self.RandomTurn()
+        GPIO.cleanup()
+        print "No way to go .. stopping.. "
+        
+  
+if __name__=="__main__":
+    GPIO.setmode(GPIO.BOARD)
+    Neo=Engine(29,31,33,35,0.5,20.0,22,21,24,23)
+    Neo.RandomRun()
+
