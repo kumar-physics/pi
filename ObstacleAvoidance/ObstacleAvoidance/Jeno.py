bbb17212c4b78727da29c307e7e949998be6789f
Kumaran Baskaran <kumar.physics@gmail.com>
2017-09-29 05:01:11 -0500

    update5

--- ObstacleAvoidance/ObstacleAvoidance/Jeno.py
+++ ObstacleAvoidance/ObstacleAvoidance/Jeno.py
@@ -130,25 +130,25 @@ class Engine(object):
             self.Scan()
             if self.status in ["s","x","t"] and self.FS.distance > self.DistanceCutoff:
                 self.MoveForward()
-                print self.FS.distance,self.BS.distance,"Moving forward"
+                print self.status,self.DistanceCutoff,self.FS.distance,self.BS.distance,"Moving forward"
                 self.turns=0
         #elif self.status in ["s","x","t"] and self.FS.distance > self.DistanceCutoff and self.BS.distance > self.DistanceCutoff :
         #    self.MoveForward()
         #    self.turns = 0
             elif self.status in ["s","x","t"] and self.BS.distance > self.DistanceCutoff:
                 self.MoveBackward()
-                print self.FS.distance,self.BS.distance,"Moving backward"
+                print self.status,self.DistanceCutoff,self.FS.distance,self.BS.distance,"Moving backward"
                 self.turns = 0
             elif self.status == "f" and self.FS.distance<self.DistanceCutoff:
             #self.Stop()
-                print self.FS.distance,self.BS.distance,"Turning"
+                print self.status,self.DistanceCutoff,self.FS.distance,self.BS.distance,"Turning"
                 self.Turn()
             elif self.status == "r" and self.BS.distance<self.DistanceCutoff:
             #self.Stop()
-                print self.FS.distance,self.BS.distance,"Turning"
+                print self.status,self.DistanceCutoff,self.FS.distance,self.BS.distance,"Turning"
                 self.Turn()
             else:
-                print self.FS.distance,self.BS.distance,"Again Turning"
+                print self.status,self.DistanceCutoff,self.FS.distance,self.BS.distance,"Again Turning"
                 self.turns+=1
                 self.Turn()
             if self.turns > self.Maxturns:
