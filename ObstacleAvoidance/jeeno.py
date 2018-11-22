edf8ea7b40720068f3022d0ec3331b374eef0b92
Kumaran Baskaran <kumar.physics@gmail.com>
2018-11-21 22:59:39 -0600

    update from mac

--- ObstacleAvoidance/jeeno.py
+++ ObstacleAvoidance/jeeno.py
@@ -11,7 +11,10 @@ class Jeeno:
 
     def move(self):
         self.front_sensor.when_out_of_range = self.robot.forward
-        self.front_sensor.when_in_range = self.robot.stop
+        self.front_sensor.when_in_range = self.turn
+
+    def turn(self):
+        self.robot.stop()
         if choice(['L','R']) == 'L':
             self.left()
         else:
